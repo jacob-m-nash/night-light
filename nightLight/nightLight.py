d0bc3590c5c82f47423a79539339eb3654b7362f
J N <jacob.matthew.nash@gmail.com>
2022-05-02 20:47:28 +0100

    added suport for datetime and more tests

--- nightLight/nightLight.py
+++ nightLight/nightLight.py
@@ -1,10 +1,22 @@
 import math
+import datetime
+
+
+def calculateSunset(date,latitude,longitude):
+    dayOfTheYear = date.timetuple().tm_yday
+    sunsetTime = calculateSunsetTime(dayOfTheYear,latitude,longitude)
+    test = date.replace(hour= sunsetTime.hour, minute = sunsetTime.minute)
+
+    return test
+
 
 # Calculates the time of day the sun will set in minutes 
-def calculateSunset(dayOfTheYear, latitude, longitude) : 
-    fractionalYear = (2 * math.pi / 360) * (dayOfTheYear - 1)
+def calculateSunsetTime(dayOfTheYear, latitude, longitude) : 
+    fractionalYear = (2 * math.pi / 360) * (dayOfTheYear -1)
     eqtime = 229.18 * (0.000075 + 0.001868 * math.cos(fractionalYear) - 0.032077 * math.sin(fractionalYear) - 0.014615 * math.cos(2 * fractionalYear) - 0.040849 * math.sin(2 * fractionalYear))
-    decl = 0.006918 - 0.399912 * math.cos(fractionalYear) + 0.070257 * math.sin(fractionalYear) - 0.006758 * math.cos(2 * fractionalYear) + 0.000907 * math.sin(2 * fractionalYear) - 0.002697 * math.cos(3 * fractionalYear) + 0.00148 * math.sin(3 * fractionalYear)  
-    hourAngle = - math.acos((math.cos(math.radians(90.833)) / (math.cos(latitude) * math.cos(decl))) - (math.tan(latitude) * math.tan(decl)))
-    sunset = 720 - 4 * (longitude +  math.degrees(hourAngle)) - eqtime
-    return sunset
+    decl = 0.006918 - 0.399912 * math.cos(fractionalYear) + 0.070257 * math.sin(fractionalYear) - 0.006758 * math.cos(2 * fractionalYear) + 0.000907 * math.sin(2 * fractionalYear) - 0.002697 * math.cos(3 * fractionalYear) + 0.00148 * math.sin(3 * fractionalYear)
+    hourAngle = - math.acos((math.cos(math.radians(90.833)) / (math.cos(math.radians(latitude)) * math.cos(decl))) - (math.tan(math.radians(latitude)) * math.tan(decl)))
+    sunsetTime = 720 - 4 * (longitude +  math.degrees(hourAngle)) - eqtime
+    sunsetHour = round(sunsetTime // 60)
+    sunsetMinute = round(sunsetTime % 60) #todo find out why we are a couple of mins off each time 
+    return datetime.time(sunsetHour,sunsetMinute)
--- nightLight/nightLightTests.py
+++ nightLight/nightLightTests.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import unittest
 
 from nightLight import calculateSunset
@@ -6,7 +7,17 @@ from nightLight import calculateSunset
 class TestCalculateSunset(unittest.TestCase):
 
     def testJan1London(self):
-        self.assertEqual(calculateSunset(1,51.5,-0.13), 1602, "Should be 16:02")
+        date = datetime(2022,1,1)
+        self.assertEqual(calculateSunset(date,51.5,-0.13), datetime(2022, 1, 1, 16, 1), "Should be 16:01")
+
+    def testJune6London(self):
+        date = datetime(2022,6,6)
+        self.assertEqual(calculateSunset(date,51.5,-0.13), datetime(2022, 6, 6, 21 - 1, 14), "Should be 21:14") #note is utc  need to -1 (BST)
+    
+    def testDec5Sydney(self):
+        date = datetime(2022,12,5)
+        self.assertEqual(calculateSunset(date,-33.87,151.22), datetime(2022, 12, 5, 19 - 11, 58), "Should be 19:54") #note is utc  so need to -11 (AEDT)
+
 
 
 if __name__ == '__main__':
