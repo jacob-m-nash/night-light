a871d3b807f7d86b10fa1e6dce6e44dac2a7923f
jacob-m-nash <44061160+jacob-m-nash@users.noreply.github.com>
2023-07-22 16:20:57 +0100

    added shutdown command

--- src/nightLight.py
+++ src/nightLight.py
@@ -11,20 +11,31 @@ from sunset_calculator import sunsetCalculator
 DEFAULT_CONFIG_FILEPATH = os.path.join(os.getcwd(),"configs","defaultConfig.json" )
 PIPE_PATH = os.path.join(os.getcwd(),"pipes","pipe") # TODO when setting up write path of pipe to "global" config file 
 
+# color is [Hue, Saturation, Brightness, Kelvin]
+# all values are uint16, max value 65535
+WARM_WHITE = [58275, 0, 65535, 3200] # TODO why is the hue not max value?
+
 class NightLightEncoder(json.JSONEncoder):
     def default(self, obj):
         return obj.__dict__   
 
 class NightLightConfig():
-    def __init__(self,lights,latitude,longitude,sunsetOffset,maxLightBrightness,lightTemperature,transitionDuration):
+    def __init__(self,lights,latitude,longitude,sunsetOffset,lightHue,lightSaturation,lightBrightness,lightTemperature,transitionDuration):
+        # list of connected lights
         self.lights = lights
+
+        # sunset settings
         self.latitude = latitude
         self.longitude = longitude
         self.sunsetOffset = sunsetOffset
-        self.maxLightBrightness = maxLightBrightness
+        
+        # light/bulb settings
+        self.lightHue = lightHue
+        self.lightSaturation = lightSaturation
+        self.lightBrightness = lightBrightness
         self.lightTemperature = lightTemperature
         self.transitionDuration = transitionDuration 
-
+       
 
     @staticmethod
     def loadFromJson(jsonDict):
@@ -32,7 +43,7 @@ class NightLightConfig():
         for light in jsonDict["lights"]:
             lights.append(Light(light["mac_addr"],light["ip_addr"],light["service"],light["port"],light["source_id"],light["verbose"]))
 
-        return NightLightConfig(lights,jsonDict["latitude"],jsonDict["longitude"],jsonDict["sunsetOffset"],jsonDict["maxLightBrightness"],jsonDict["lightTemperature"],jsonDict["transitionDuration"])
+        return NightLightConfig(lights,jsonDict["latitude"],jsonDict["longitude"],jsonDict["sunsetOffset"],jsonDict["lightHue"],jsonDict["lightSaturation"],jsonDict["lightBrightness"],jsonDict["lightTemperature"],jsonDict["transitionDuration"])
 
 
 def loadConfig():
@@ -63,11 +74,15 @@ def getUserConfig():
     latitude = getSettingInput("Latitude", -90, 90)
     longitude = getSettingInput("Longitude", -180, 180)
     sunsetOffset = getSettingInput("Sunset Offset (mins)", 0, 720) # 720 mins = 12 hours
-    maxLightBrightness = getSettingInput("Max Brightness Setting", 0, 1)
+    lightBrightness = getSettingInput("Max Brightness Setting", 0, 1)
     lightTemperature = getSettingInput("Light Temperature (Kelvin)", 2500, 9000)
     transitionDuration = getSettingInput("Transition Duration (sec)", 0,3600) # 3600 secs = 1 hour
                 
-    return NightLightConfig(lifx.get_lights(),latitude,longitude,sunsetOffset,maxLightBrightness,lightTemperature,transitionDuration)
+    return NightLightConfig(lifx.get_lights(),latitude,longitude,sunsetOffset,lightBrightness,lightTemperature,transitionDuration)
+
+def Shutdown():
+    os.remove(PIPE_PATH)
+    exit()
 
 def Run():
     config = loadConfig()
@@ -82,20 +97,25 @@ def Run():
             if(lightTransitionStartTime < currentTime):
                 break
             currentTime = datetime.now(pytz.UTC)
-        # color is [Hue, Saturation, Brightness, Kelvin]
-        # all values are uint16, max value 65535
-        WARM_WHITE = [58275, 0, 65535 * config.maxLightBrightness, config.lightTemperature] # TODO why is the hue not max value?
-        color = WARM_WHITE
+        color = [config.lightHue,config.lightSaturation,config.lightBrightness,config.lightTemperature]
         for light in config.lights: 
-            light.set_color(color,config.transitionDuration,False)
+            light.set_color(color,config.transitionDuration)
             light.set_power("on")
 
 def waitForCommand():
+    while True:
         with open(PIPE_PATH, 'r') as pipeInput:
             input = pipeInput.readline()
+        reply = parseCommand(input)
+        if reply is None:
+            continue
         with open(PIPE_PATH, 'w') as pipeOutput:
-            returnStr = "echo: " + input
-            pipeOutput.write(returnStr)
+            pipeOutput.write(reply)
+
+def parseCommand(command,args):
+    input = str.lower(command)
+    if input == "shutdown":
+        Shutdown() 
 
 def Startup():
     print("Creating pipe")
